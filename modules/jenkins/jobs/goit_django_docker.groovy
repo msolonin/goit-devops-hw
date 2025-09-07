1547d8fe515bc913da9770e5d63f62bb8ba7d7dd
msolonin <cipabook@gmail.com>
2025-09-07 14:42:44 +0200

    drg

--- modules/jenkins/jobs/goit_django_docker.groovy
+++ modules/jenkins/jobs/goit_django_docker.groovy
@@ -7,7 +7,7 @@ pipelineJob("goit-django-docker") {
             url(${github_repo_url})
             credentials("github-token")
           }
-          branches("*/lesson-7")
+          branches("*/lesson-9")
         }
       }
       scriptPath("Jenkinsfile")
