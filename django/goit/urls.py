075488ac1568ada83de7057285c7d029aac50b80
msolonin <cipabook@gmail.com>
2025-09-14 16:49:07 +0200

    qwdqwdqw

--- django/goit/urls.py
+++ django/goit/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
+from django.urls import path
+from django_prometheus import exports
 
 def home(request):
     return HttpResponse("Hello Django!")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("metrics/", include("django_prometheus.urls")),  # note the trailing slash
+    path("metrics/", exports.ExportToDjangoView),  # <-- Prometheus metrics
     path("", home),  # optional home page
 ]
